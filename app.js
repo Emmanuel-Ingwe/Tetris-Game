38ca5c2014360ed604796b6eb1913280fb1fdec2
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-07 16:35:58 +0200

    ++

--- app.js
+++ app.js
@@ -211,7 +211,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // game over
     function gameOver() {
         if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-            scoreDisplay.innerHTML = 'end';
+            scoreDisplay.innerHTML = 'GameOverBitch!';
             clearInterval(timerId);
         }
     }
